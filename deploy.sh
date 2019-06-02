e05146361d37524cdb5f7160b764b33aef6c9230
Danilo Poccia <danilop@amazon.com>
2019-06-02 16:56:38 +0000

    Deploy script

---
+++ deploy.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+USAGE="Usage: $0 {PACKAGES_BUCKET} {STACK_NAME}"
+PACKAGES_BUCKET=${1?$USAGE}
+STACK_NAME=${3?$USAGE}
+sam build
+sam package --s3-bucket $PACKAGES_BUCKET --output-template-file packaged.yaml
+sam deploy --template-file packaged.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_IAM

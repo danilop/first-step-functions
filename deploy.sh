50c89a807957b78828d7b89b387e5a62533548a0
Danilo Poccia <danilop@amazon.com>
2019-06-03 08:16:41 +0000

    Quick fixes

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 set -e
 USAGE="Usage: $0 {PACKAGES_BUCKET} {STACK_NAME}"
 PACKAGES_BUCKET=${1?$USAGE}
-STACK_NAME=${3?$USAGE}
+STACK_NAME=${2?$USAGE}
 sam build
 sam package --s3-bucket $PACKAGES_BUCKET --output-template-file packaged.yaml
 sam deploy --template-file packaged.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_IAM

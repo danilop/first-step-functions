9bba73c64f7e7ea10c6f8c2b6cf9c008bf54c6ab
Danilo Poccia <dpoccia@gmail.com>
2019-06-11 12:38:33 +0100

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,7 @@ set -e
 USAGE="Usage: $0 {PACKAGES_BUCKET} {STACK_NAME}"
 PACKAGES_BUCKET=${1?$USAGE}
 STACK_NAME=${2?$USAGE}
+sam validate
 sam build
 sam package --s3-bucket $PACKAGES_BUCKET --output-template-file packaged.yaml
 sam deploy --template-file packaged.yaml --stack-name $STACK_NAME --capabilities CAPABILITY_IAM
